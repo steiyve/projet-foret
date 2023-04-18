2ce6bcceea77f882cde87e220fc6318a50a62a9b
steiyve <94018684+steiyve@users.noreply.github.com>
2023-04-18 10:05:26 -0400

    la v1 sur le crack

--- foret.py
+++ foret.py
@@ -13,7 +13,7 @@ image_path = ""
 def demande():
     global image_path
     nbSapin = input("vous voulez tous beaucoup de sapin: ")
-    path = ["picpick_39980-preview.jpeg", "téléchargement (1).jpg"]
+    path = ["image/picpick_39980-preview.jpeg", "image/téléchargement (1).jpg"]
     #de la logique pour les sapin
     if nbSapin == "oui":
         image_path = path[1]
@@ -42,6 +42,6 @@ while running:
             running = False
 
 #on quite
-print("les sources pour les image:https://www.picandpick.com/soundwave-on-the-road/photos/39980-presque-%C3%AEle-gonfl%C3%A9e-de-sapins-sur-un-lac-bleu-turquoise- et https://fr.123rf.com/photo_76842552_touriste-visitant-le-lac-de-montagne-dans-la-for%C3%AAt-de-sapins-verts-dans-un-paysage-d-%C3%A9t%C3%A9-pittoresque-.html")
+print("les source pour les image sont dans source.txt")
 pygame.quit()
 sys.exit()
